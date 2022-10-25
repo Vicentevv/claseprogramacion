0801da1b8fcead462071fbb677d2914b3dbe1951
Vicentevv <vicenteval759@gmail.com>
2022-10-25 12:37:02 -0500

    Tarea de comandos de git

---
+++ src/Git.java
@@ -0,0 +1,28 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Git");
+        System.out.println("Colocar los comandos git utilizados desde la inicialización del proyecto a  git hasta la colocación de los archivos en github");
+        System.out.print("1. Public Static Void Main (psvm) ");
+        System.out.println("Es una función principal en Java para ejecutar una clase, como una aplicación de consola.");
+        System.out.print("2. System.out.println (sout)");
+        System.out.println("Imprime el texto especificado dentro de los paréntesis en la pantalla . especificado dentro de los paréntesis en la pantalla.");
+        System.out.print("Git init");
+        System.out.println("El comando git init crea un nuevo repositorio de Git. Puede utilizarse para convertir un proyecto existente y sin versión en un repositorio de Git, o para inicializar un nuevo repositorio vacío.");
+        System.out.print("Git status.");
+        System.out.println("El comando git status muestra el estado del directorio de trabajo y del área del entorno de ensayo. Permite ver los cambios que se han preparado, los que no y los archivos en los que Git no va a realizar el seguimiento.");
+        System.out.print("Git add .");
+        System.out.println("El comando git add añade un cambio del directorio de trabajo en el entorno de ensayo. De este modo, indica a Git que quieres incluir actualizaciones en un archivo concreto en la próxima confirmación.");
+        System.out.print("Git commit -m 'first commit'");
+        System.out.println("El comando git commit captura una instantánea de los cambios preparados en ese momento del proyecto. Las instantáneas confirmadas pueden considerarse como versiones \"seguras\" de un proyecto: Git no las cambiará nunca a no ser que se lo pidas expresamente.");
+        System.out.print("Git config --global user.email");
+        System.out.println("El comando git config es una función útil que sirve para definir valores de configuración de Git a nivel de un proyecto global o local. Estos niveles de configuración se corresponden con archivos de texto . gitconfig . Al ejecutar git config , se modificará un archivo de texto de configuración.");
+        System.out.print("Git config --global user.name");
+        System.out.println("El comando Git config --global user.name es una función útil que sirve para definir valores de configuración de tu nombre de usuario de Git");
+        System.out.print("Git branck -M main");
+        System.out.println("El comando git branch es en realidad una especie de herramienta de gestión de ramas. Puede listar las ramas que tienes, crear una nueva rama, eliminar ramas y cambiar el nombre de las ramas.");
+        System.out.print("Git remote add origin https://github.com/Vicentevv/clasep");
+        System.out.println("El comando git remote te permite crear, ver y eliminar conexiones con otros repositorios. Las conexiones remotas se asemejan más a marcadores que a enlaces directos con otros repositorios.");
+        System.out.print("Git push -u origin main");
+        System.out.println("El comando git push se usa para cargar contenido del repositorio local a un repositorio remoto. El envío es la forma de transferir confirmaciones desde tu repositorio local a un repositorio remoto");
+    }
+}
